08431d302fa28cb14d5449c8319811d1213a55ed
Brian Dizon <bvdizon@gmail.com>
2022-07-25 11:04:30 +0800

    added NE calgary

---
+++ content/northeast.php
@@ -0,0 +1,111 @@
+<div class="breadcrumbs">
+    <ol>
+        <li><a href="/">Calgary Real Estate</a></li>
+        <li>NorthEast</li>
+    </ol>
+</div>
+<h1>Houses for Sale in Calgary NE</h1>
+<p>Pick your Category or Housing Price first then Pick your Chosen Community</p>
+
+<p><strong>Search all Calgary NorthEast houses for sale <a href="#listings" class="primary-color">listed within the past
+            24 hours</a>.</strong> The Northeast part of Calgary offers buyers affordability within the competitive
+    Calgary housing market. Access to
+    this area has been made easier recently with the expansion of the Stony Trail Expressway. More housing projects,
+    developmental opportunities and business areas are popping up every month. 47 communities as well as Calgary&rsquo;s
+    International Airport are nestled within Northeast Calgary and whether you are looking to purchase a Calgary home or
+    settle into a Calgary condominium, this area is sure to please.</p>
+
+<p>#community-links-northeast#</p>
+
+
+<div id="listings" style="margin: 3rem 0;">#all-ne-new-listings#</div>
+
+<div class="highlight">
+    <p>The Calgary Real Estate Board (CREB&reg;) is aligning it's subdivision names and boundaries with the City of Calgary community names and districts to maintain clarity and consistency.</p>
+    <p> If you are searching for a community which does not appear on this list, please see our page for <a href="/old-calgary-communities.php"><strong>Old Calgary Communities.</strong></a></p>
+    <p>To view all houses for sale listed within the past 24, please see our page <a href="/calgary-newlistingslessthan24hoursold.php"><strong>Calgary's Brand New Listings for Sale</strong></a>.</p>
+</div>
+
+<div class="communities-nav">Select <em>Specific Communities</em> below, use the <em>form above</em> to search more than
+    one community at a time. Or you can search <a title="Calgary Listings Search" href="/idx/">all the Calgary MLS&reg;
+        listings</a> available. THIS SITE IS UPDATED DIRECTLY FROM MLS&reg; SEVERAL TIMES PER DAY. <br /><br />
+    <ul class="nav">
+        <li><a href="/abbeydale.php">Abbeydale</a></li>
+        <li><a href="/castleridge.php">Castleridge</a></li>
+        <li><a href="/cityscape.php">Cityscape</a></li>
+        <li><a href="/coral-springs.php">Coral Springs</a></li>
+        <li><a href="/falconridge.php">Falconridge</a></li>
+        <li><a href="/homestead.php">Homestead</a></li>
+        <li><a href="/marlborough.php">Marlborough</a></li>
+        <li><a href="/marlborough-park.php">Marlborough Park</a></li>
+        <li><a href="/martindale.php">Martindale</a></li>
+        <li><a href="/mayland-heights.php">Mayland Heights</a></li>
+        <li><a href="/monterey-park.php">Monterey Park</a></li>
+        <li><a href="/pineridge.php">Pineridge</a></li>
+        <li><a href="/redstone.php">Redstone</a></li>
+        <li><a href="/rundle.php">Rundle</a></li>
+        <li><a href="/saddle-ridge.php">Saddle Ridge</a></li>
+        <li><a href="/skyview-ranch.php">Skyview Ranch</a></li>
+        <li><a href="/taradale.php">Taradale</a></li>
+        <li><a href="/temple.php">Temple</a></li>
+        <li><a href="/vista-heights.php">Vista Heights</a></li>
+        <li><a href="/whitehorn.php">Whitehorn</a></li>
+    </ul>
+</div>
+<script type="application/ld+json">
+// <![CDATA[
+{
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    "itemListElement": [{
+            "@type": "ListItem",
+            "position": 1,
+            "name": "Calgary Real Estate | Calgary Homes for Sale & Neighbourhood Info",
+            "item": "https://www.calgary-real-estate.com/"
+        },
+        {
+            "@type": "ListItem",
+            "position": 2,
+            "name": "MLS® | Calgary NE | NorthEast Calgary",
+            "item": "https://www.calgary-real-estate.com/northeast.php"
+        }
+    ]
+}
+// ]]>
+</script>
+<!-- schema - organization -->
+<script type="application/ld+json">
+// <![CDATA[
+{
+    "@context": "http://www.schema.org",
+    "@type": "Organization",
+    "name": "The Group at RE/MAX FIRST",
+    "url": "https://www.calgary-real-estate.com/",
+    "logo": "https://www.calgary-real-estate.com/uploads/logo_content_page.71.webp",
+    "image": "https://www.calgary-real-estate.com/uploads/agent-1/the-group/team-photo-the-group-at-remax-first-v1.jpg",
+    "description": "Voted Best Real Estate Website by the Web Marketing Association in 2020. Find community information, search homes for sale and research pricing and details on Calgary-Real-Estate.com.",
+    "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "#115 8820 Blackfoot Trail SE",
+        "addressLocality": "Calgary",
+        "addressRegion": "Alberta",
+        "postalCode": "T2J 3J1",
+        "addressCountry": "Canada"
+    },
+    "geo": {
+        "@type": "GeoCoordinates",
+        "latitude": "51.001490",
+        "longitude": "-114.048660"
+    },
+    "hasMap": "https://goo.gl/maps/UJ4dkoo8E7qzC3at7",
+    "openingHours": "Mo, Tu, We, Th, Fr 08:00-21:00 Sa, Su 09:00-21:00",
+    "contactPoint": {
+        "@type": "ContactPoint",
+        "telephone": "+1(403) 255-9555",
+        "contactType": "general"
+    }
+}
+// ]]>
+</script>
+<!-- @end: schema - organization -->
+<div>#styles-conversion-optimization#</div>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include 'template-parts/header.php'; ?>
 <!-- Entry Content  -->
-<?php include 'content/north.php'; ?>
+<?php include 'content/northeast.php'; ?>
 <!-- @end  Entry Content  -->
 <?php include 'template-parts/footer-with-sidebar.php'; ?>
